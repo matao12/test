35f186ab1bc3b95ba80634af19ba6401d7d3355f
matao12 <16621269430@163.com>
2019-10-08 17:37:17 +0800

    two

---
+++ 10_8/app/src/main/java/com/example/a10_8/Main2Activity.java
@@ -0,0 +1,17 @@
+package com.example.a10_8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
+
+
+
--- 10_8/app/src/main/java/com/example/a10_8/MainActivity.java
+++ 10_8/app/src/main/java/com/example/a10_8/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.a10_8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button button = findViewById(R.id.main_1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
 
 //        666
+        //two
     }
 }
